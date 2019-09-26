1435eaead8d6e3991df61deebe23c92ab92fff0f
the1mills <the1mills@yahoo.com>
2019-09-26 15:39:13 -0700

    tmp

--- assets/postinstall.sh
+++ assets/postinstall.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-set -eo pipefail;
+set -e;
 
 if [ "$skip_postinstall" = "yes" ]; then
     echo "skipping postinstall routine.";
--- scripts/tsc/tests.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-cd tests;
-tsc -p tsconfig.test.json --watch
