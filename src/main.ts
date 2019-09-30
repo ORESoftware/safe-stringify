7a2518ce809a6ed293d608bf8aa8da6d39e27d44
the1mills <the1mills@yahoo.com>
2019-09-30 14:47:48 -0700

    tmp

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,68 @@ export const stringify = (v: object | boolean | number): string => {
   });
 };
 
+const mapper = (cache: Set<any>, key: any, val: any): any => {
+  
+  if (val && typeof val === 'object') {
+    
+    if (cache.has(val)) {
+      
+      if (val instanceof Buffer) {
+        return {
+          type: 'Buffer',
+          val: Array.from(<Buffer>val).map((v, i) => mapper(cache, i, v))
+        }
+      }
+      
+      if (val instanceof Map) {
+        return {
+          type: 'Map',
+          val: Array.from(<Map<any, any>>val).map((v, i) => mapper(cache, i, v))
+        }
+      }
+      
+      if (val instanceof Set) {
+        return {
+          type: 'Set',
+          val: Array.from(<Set<any>>val).map((v, i) => mapper(cache, i, v))
+        }
+      }
+      
+      if (val instanceof RegExp) {
+        return {
+          type: 'RegExp',
+          val: String(<RegExp>val)
+        }
+      }
+      
+      if(Array.isArray(val)){
+        return (<Array<any>>val).map((v, i) => mapper(cache, i, v))
+      }
+      
+      // Circular reference found, discard key
+      try {
+        // If this value does not reference a parent it can be deduped
+        return JSON.parse(JSON.stringify(val));
+      }
+      catch (err) {
+        // discard key if value cannot be deduped
+        return;
+      }
+    }
+    // Store value in our map
+    cache.add(val);
+  }
+  
+  return val;
+};
+
+export const stringifyAll = (v: object | boolean | number): string => {
+  const cache = new Set<any>();
+  return JSON.stringify(v, (key, val) => {
+    return mapper(cache, key, val);
+  });
+};
+
 const parentSym = Symbol('parent');
 const cacheSym = Symbol('cache');
 
