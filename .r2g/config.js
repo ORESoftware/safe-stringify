b3dc3d62228fa72aaaa6281ede55ce719ba1e8f5
Alexander Mills <alexander.d.mills@gmail.com>
2026-06-02 19:39:30 -0500

    expand r2g phase coverage

--- .r2g/config.js
+++ .r2g/config.js
@@ -1,19 +1,18 @@
-'use strict';
-
-// NOTE : the only dependencies you should import here are core/built-in modules
 const path = require('path');
 
-const searchRoot = path.resolve(process.env.R2G_SEARCH_ROOT || process.env.HOME || '');
+const searchRoot = path.resolve(
+  process.env.R2G_SEARCH_ROOT ||
+  process.env.MY_DOCKER_R2G_SEARCH_ROOT ||
+  process.env.HOME ||
+  ''
+);
 
 if (!path.isAbsolute(searchRoot)) {
-  throw new Error('Please set the env var "MY_DOCKER_R2G_SEARCH_ROOT" to an absolute folder path,' +
-    ' (note that user $HOME is usually not specific enough, and also that $HOME env var is empty).');
+  throw new Error('Please set R2G_SEARCH_ROOT or MY_DOCKER_R2G_SEARCH_ROOT to an absolute folder path.');
 }
 
 exports.default = {
-
   searchRoot,
   tests: '',
   packages: {}
-
 };
